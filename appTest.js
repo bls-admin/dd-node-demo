fe4096cc7834842d490651ce3b263f57b6b4d490
AnilkumarGT <anilkumar.gt@gmail.com>
2017-05-13 19:24:32 +0530

    changed tests

--- appTest.js
+++ appTest.js
@@ -30,6 +30,7 @@ app.use(function(req, res, next) {
 // error handlers
 
 // development error handler
+// will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
